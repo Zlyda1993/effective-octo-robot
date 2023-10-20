293612afb613e1910576acad5130fd5002dae6ff
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-10-20 14:06:52 -0400

    Set up post comment route.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -67,6 +67,19 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const commentData = await Comment.destroy({
